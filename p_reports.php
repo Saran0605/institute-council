e08b8cac78f06efc9096d142cf9aa96ea1bf32e0
Saran0605 <saransoundharajan05@gmail.com>
2024-10-28 16:46:08 +0530

    reports table removed

--- p_reports.php
+++ p_reports.php
@@ -134,38 +134,7 @@
                 </div>
             </div>
             <div class="container-fluid">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table id="report1" class="table table-striped table-bordered">
-                                <thead style="background-color:black;color:white">
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Position</th>
-                                        <th>Office</th>
-                                        <th>Age</th>
-                                        <th>Start date</th>
-                                        <th>Salary</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Tiger Nixon</td>
-                                        <td>System Architect</td>
-                                        <td>Edinburgh</td>
-                                        <td>61</td>
-                                        <td>2011/04/25</td>
-                                        <td>$320,800</td>
-                                    </tr>
-
-                                </tbody>
-
-                            </table>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
+               
 
 
         </div>
