b79a1be28abb4ed23e49ecf3e78bc03aa0fea5c4
vishwangar <vishwangarravikumar@gmail.com>
2024-11-04 15:27:50 +0530

    file modified

--- p_funds.php
+++ p_funds.php
@@ -1,12 +1,7 @@
 <?php
 include "db.php";
-<<<<<<< HEAD
 $sql="SELECT * FROM fundedprojects";
 $result=$conn->query($sql);
-=======
-$sql = "SELECT * FROM fundedprojects";
-$result = $conn->query($sql);
->>>>>>> 05b22b6e62120fa98936183c434fa2ddbd50a90e
 ?>
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
@@ -61,7 +56,6 @@ $result = $conn->query($sql);
         .table {
             margin-top: 10px;
         }
-
     </style>
 </head>
 
@@ -155,19 +149,18 @@ $result = $conn->query($sql);
 
                     <ul class="nav nav-pills bg-nav-pills nav-justified my-3" id="researchTab" role="tablist">
                         <li class="nav-item" role="presentation">
-                            <a class="nav-link rounded-0 active nav-bas" id="Entries-tab" data-toggle="pill" href="#Entries" role="tab" aria-controls="Entries" aria-selected="true">Entries</a>
+                            <a class="nav-link rounded-0 active nav-bas" id="targets-tab" data-toggle="pill" href="#targets" role="tab" aria-controls="targets" aria-selected="true">Targets</a>
                         </li>
                         <li class="nav-item" role="presentation">
-                            <a class="nav-link rounded-0" id="Reports-tab" data-toggle="pill" href="#Reports" role="tab" aria-controls="Reports" aria-selected="false">Reports</a>
+                            <a class="nav-link rounded-0" id="journal-tab" data-toggle="pill" href="#journal" role="tab" aria-controls="journal" aria-selected="false">Journal</a>
                         </li>
                         <li class="nav-item" role="presentation">
-                            <a class="nav-link rounded-0" id="consultancy-tab" data-toggle="pill" href="#consultancy" role="tab" aria-controls="consultancy" aria-selected="false">Statistics</a>
+                            <a class="nav-link rounded-0" id="consultancy-tab" data-toggle="pill" href="#consultancy" role="tab" aria-controls="consultancy" aria-selected="false">Consultancy</a>
                         </li>
                     </ul>
                 </div>
                 <div class="card">
                     <div class="tab-content" id="researchTabContent">
-<<<<<<< HEAD
                         <div class="tab-pane fade show active" id="targets" role="tabpanel" aria-labelledby="targets-tab">
                             <br>
                             <table id="journalTable" class="table table-striped table-bordered table-responsive">
@@ -208,90 +201,11 @@ $result = $conn->query($sql);
                                 </tbody>
 
                             </table>
-=======
-                        <div class="tab-pane fade show active" id="Entries" role="tabpanel" aria-labelledby="Entries-tab">
-                            <br>
-                            <div style="overflow-x: auto;">
-                                <table id="EntriesTable" class="table table-striped table-bordered">
-                                    <thead style="background-color:black;color:white;width:50px;">
-                                        <tr>
-                                            <th>Faculty Name</th>
-                                            <th>Faculty ID</th>
-                                            <th>Title</th>
-                                            <th>Type</th>
-                                            <th>Project ID</th>
-                                            <th>Funding Agency</th>
-                                            <th>Link</th>
-                                            <th>Requested Amount</th>
-                                            <th>Status</th>
-                                            <th>Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        if ($result->num_rows > 0) {
-                                            while ($row = $result->fetch_assoc()) { ?>
-                                                <tr>
-                                                    <td><?php echo $row['faculty_name']; ?></td>
-                                                    <td><?php echo $row['faculty_id']; ?></td>
-                                                    <td><?php echo $row['title']; ?></td>
-                                                    <td><?php echo $row['type']; ?></td>
-                                                    <td><?php echo $row['projectid']; ?></td>
-                                                    <td><?php echo $row['fundingagency']; ?></td>
-                                                    <td><?php echo $row['link']; ?></td>
-                                                    <td><?php echo $row['requestedamount']; ?></td>
-                                                    <td><?php echo $row['status']; ?></td>
-                                                    <td><a class="btn btn-info" href="update.php?id=<?php echo $row['id']; ?>">Edit</a>&nbsp;
-                                                        <a class="btn btn-danger" href="delete.php?id=<?php echo $row['id'] ?>">Delete</a>
-                                                    </td>
-                                                </tr>
-                                        <?php }
-                                        } ?>
-                                    </tbody>
-                                </table>
-                            </div>
->>>>>>> 05b22b6e62120fa98936183c434fa2ddbd50a90e
                             <br>
                         </div>
 
-                        <div class="tab-pane fade" id="Reports" role="tabpanel" aria-labelledby="Reports-tab">
-                        <br>
-                            <table id="ReportsTable" class="table table-striped table-bordered">
-                                <thead style="background-color:black;color:white;width:50px;">
-                                    <tr>
-                                        <th>S.No</th>
-                                        <th>Department Name</th>
-                                        <th>Faculty count</th>
-                                        <th>Total Count</th>
-                                        <th>This Month Target</th>
-                                        <th>This Month Count</th>
-                                        <th>Progress</th>
-                                        <th>Deviations</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    $s = 1;
-                                    while ($row = mysqli_fetch_assoc($result)) {
-                                    ?><tr>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                        </tr>
-                                    <?php
-                                        $s++;
-                                    }
-                                    ?>
-                                </tbody>
-
-                            </table>
-                            <br>
-                        
+                        <div class="tab-pane fade" id="journal" role="tabpanel" aria-labelledby="journal-tab">
+                            <!-- table veganum  -->
                         </div>
 
                         <div class="tab-pane fade" id="consultancy" role="tabpanel" aria-labelledby="consultancy-tab">
@@ -319,8 +233,8 @@ $result = $conn->query($sql);
     <script>
         $(document).ready(function() {
             // Initialize DataTables for each table
-            $('#ReportsTable').DataTable();
-            $('#EntriesTable').DataTable();
+            $('#journalTable').DataTable();
+            $('#consultancyTable').DataTable();
         });
     </script>
 
