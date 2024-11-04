47608e7a38ea4ad304aa3c269dcb016ef89bc29f
NalinKumarSP <nalinkumar5814@gmail.com>
2024-11-04 11:10:48 +0530

    report kojam velaii

--- p_reports.php
+++ p_reports.php
@@ -174,8 +174,10 @@
 
 
                 <div class="tab-content">
-                    <div class="tab-pane fade show active" id="rag1" role="tabpanel" aria-labelledby="rag1-tab">
-                        <h3> Faculty Report</h3>
+                    <div class="tab-pane fade show active card" id="rag1" role="tabpanel" aria-labelledby="rag1-tab">
+                        &nbsp;
+                        <h4 style="margin-left: 10px; color:black;"> Faculty Report</h4>
+                        <br>
                         <table id="report1" class="table table-striped table-bordered">
                             <thead style="background-color:black;color:white">
                                 <tr>
@@ -191,11 +193,14 @@
                             </thead>
 
                         </table>
+                        <br>
                     </div>
 
 
-                    <div class="tab-pane fade" id="rag2" role="tabpanel" aria-labelledby="rag2-tab">
-                        <h3>Faculty Wise Report</h3>
+                    <div class="tab-pane fade card" id="rag2" role="tabpanel" aria-labelledby="rag2-tab">
+                        &nbsp;
+                        <h4 style="margin-left: 10px; color:black;">Faculty Wise Report</h4>
+                        <br>
                         <table id="report2" class="table table-striped table-bordered">
                             <thead style="background-color:black;color:white">
                                 <tr>
@@ -215,6 +220,7 @@
                             </thead>
 
                         </table>
+                        <br>
                     </div>
 
                 </div>
