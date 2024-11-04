c4ed77882775571d7d224e3d0e3e57d1f359ec89
NalinKumarSP <nalinkumar5814@gmail.com>
2024-11-04 09:10:10 +0530

    nalin table with fixed

--- p_journal.php
+++ p_journal.php
@@ -4,43 +4,60 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
     <title>Institute Council</title>
-    <!-- Custom CSS -->
     <link href="assets/libs/flot/css/float-chart.css" rel="stylesheet">
-    <!-- Custom CSS -->
     <link href="dist/css/style.min.css" rel="stylesheet">
-    <!--   datatable link -->
     <link rel="stylesheet" type="text/css" href="assets/extra-libs/multicheck/multicheck.css">
     <link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css" rel="stylesheet">
 
+    <style>
+        .bg-nav-pills {
+            background-color: #f8f9fa;
+        }
 
+        .nav-pills .nav-link {
+            color: #000;
+            font-weight: 500;
+            padding: 12px 2x;
+        }
 
+        .nav-pills .nav-link.active {
+            background-color: #007bff;
+            color: #fff;
+        }
+
+        .nav-link:hover {
+            background-color: #007bff;
+            color: #fff;
+            transition: 0.3s;
+        }
+
+        .nav-justified .nav-item {
+            flex: 1;
+            text-align: center;
+        }
+
+        /* table class name */
+        .my-3 {
+            margin-top: 1rem !important;
+            margin-bottom: 1rem !important;
+        }
+    </style>
 </head>
 
 <body>
-    <div class="preloader">
-        <div class="lds-ripple">
-            <div class="lds-pos"></div>
-            <div class="lds-pos"></div>
-        </div>
-    </div>
     <div id="main-wrapper">
         <header class="topbar" data-navbarbg="skin5">
             <nav class="navbar top-navbar navbar-expand-md navbar-dark">
                 <div class="navbar-header" data-logobg="skin5">
-                    <!-- This is for the sidebar toggle which is visible on mobile only -->
                     <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
                     <a class="navbar-brand" href="index.html">
-                        <!-- Logo icon -->
                         <b class="logo-icon p-l-10">
                             <img src="assets/images/logo-icon.png" alt="homepage" class="light-logo" />
-
                         </b>
                         <span class="logo-text">
                             <img src="assets/images/logo-text.png" alt="homepage" class="light-logo" />
@@ -64,15 +81,13 @@
             </nav>
         </header>
         <aside class="left-sidebar" data-sidebarbg="skin5">
-            <!-- Sidebar scroll-->
             <div class="scroll-sidebar">
-                <!-- Sidebar navigation-->
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav" class="p-t-30">
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="principal.php" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span></a></li>
 
                         <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu">Research</span></a>
-                            <ul aria-expanded="false" class="collapse  first-level">
+                            <ul aria-expanded="false" class="collapse first-level">
                                 <li class="sidebar-item"><a href="p_reports.php" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Reports</span></a></li>
                                 <li class="sidebar-item"><a href="p_journal.php" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu">Journal</span></a></li>
                                 <li class="sidebar-item"><a href="p_consultancy.php" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu">Consultancy</span></a></li>
@@ -103,78 +118,121 @@
                 </div>
             </div>
             <div class="container-fluid">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table id="journal1" class="table table-striped table-bordered" style="width:fit-content">
-                                <thead style="background-color:black;color:white">
-                                    <tr>
-                                        <th>Faculty Name</th>
-                                        <th> Faculty ID</th>
-                                        <th>Type</th>
-                                        <th>Title</th>
-                                        <th>Volume</th>
-                                        <th> Issue Number</th>
-                                        <th>Pages</th>
-                                        <th>Date</th>
-                                        <th>Link</th>
-                                        <th>No of Author</th>
-                                        <th> Authors Position</th>
-                                        <th>Status</th>
-                                        <th>File</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                   
 
-                                </tbody>
+                <div style="width: 100%;">
+                    <ul class="nav nav-pills bg-nav-pills nav-justified my-3" id="researchTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link rounded-0 active nav-bas" id="targets-tab" data-toggle="pill" href="#targets" role="tab" aria-controls="targets" aria-selected="true">Targets</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link rounded-0" id="journal-tab" data-toggle="pill" href="#journal" role="tab" aria-controls="journal" aria-selected="false">Journal</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link rounded-0" id="consultancy-tab" data-toggle="pill" href="#consultancy" role="tab" aria-controls="consultancy" aria-selected="false">Consultancy</a>
+                        </li>
+                    </ul>
+                    <div class="card">
+                        <div class="tab-content" id="researchTabContent">
+                            <div class="tab-pane fade show active" id="targets" role="tabpanel" aria-labelledby="targets-tab">
+                                <table id="funds1" class="table table-striped table-bordered">
+                                    <thead style="background-color:black;color:white;width:50px;">
+                                        <tr>
+                                            <th>Faculty Name</th>
+                                            <th>Faculty ID</th>
+                                            <th>Tittle</th>
+                                            <th>Type</th>
+                                            <th>Project ID</th>
+                                            <th>Funding Agency</th>
+                                            <th>Link</th>
+                                            <th>Requested Amount</th>
+                                            <th>Status</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
 
-                            </table>
+
+
+                                    </tbody>
+
+                                </table>
+                            </div>
+
+                            <div class="tab-pane fade" id="journal" role="tabpanel" aria-labelledby="journal-tab">
+                                <table class="table table-striped mt-3" id="journalTable">
+                                    <thead>
+                                        <tr>
+                                            <th>Journal Title</th>
+                                            <th>Author</th>
+                                            <th>Publication Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Journal 1</td>
+                                            <td>Author 1</td>
+                                            <td>2024/06/15</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Journal 2</td>
+                                            <td>Author 2</td>
+                                            <td>2024/07/20</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div class="tab-pane fade" id="consultancy" role="tabpanel" aria-labelledby="consultancy-tab">
+                                <table class="table table-striped mt-3" id="consultancyTable">
+                                    <thead>
+                                        <tr>
+                                            <th>Consultant</th>
+                                            <th>Project</th>
+                                            <th>Duration</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Consultant 1</td>
+                                            <td>Project 1</td>
+                                            <td>6 Months</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Consultant 2</td>
+                                            <td>Project 2</td>
+                                            <td>1 Year</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-
                     </div>
                 </div>
-
             </div>
             <footer class="footer text-center">
                 2024 © M.Kumarasamy College of Engineering All Rights Reserved.
             </footer>
         </div>
     </div>
+
+    <!-- Scripts -->
     <script src="assets/libs/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap tether Core JavaScript -->
     <script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
     <script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
     <script src="assets/extra-libs/sparkline/sparkline.js"></script>
-    <!--Wave Effects -->
     <script src="dist/js/waves.js"></script>
-    <!--Menu sidebar -->
     <script src="dist/js/sidebarmenu.js"></script>
-    <!--Custom JavaScript -->
     <script src="dist/js/custom.min.js"></script>
-    <!--This page JavaScript -->
-    <!-- <script src="dist/js/pages/dashboards/dashboard1.js"></script> -->
-    <!-- Charts js Files -->
-    <script src="assets/libs/flot/excanvas.js"></script>
-    <script src="assets/libs/flot/jquery.flot.js"></script>
-    <script src="assets/libs/flot/jquery.flot.pie.js"></script>
-    <script src="assets/libs/flot/jquery.flot.time.js"></script>
-    <script src="assets/libs/flot/jquery.flot.stack.js"></script>
-    <script src="assets/libs/flot/jquery.flot.crosshair.js"></script>
-    <script src="assets/libs/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
-
-    <!--   datatable link -->
-
     <script src="assets/extra-libs/DataTables/datatables.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <script>
         $(document).ready(function() {
-            $('#journal1').DataTable();
+            // Initialize DataTables for each table
+            $('#journalTable').DataTable();
+            $('#consultancyTable').DataTable();
         });
     </script>
-
 </body>
 
 </html>
