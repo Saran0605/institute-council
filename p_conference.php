8f91afdc5db40d776614b81f7ea58b16d4fea642
NalinKumarSP <nalinkumar5814@gmail.com>
2024-11-04 11:00:10 +0530

    7 deleted

--- p_conference.php
+++ p_conference.php
@@ -69,7 +69,7 @@
                 <div class="navbar-header" data-logobg="skin5">
                     <!-- This is for the sidebar toggle which is visible on mobile only -->
                     <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i
-                            class="ti-menu ti-close"></i></a>7
+                            class="ti-menu ti-close"></i></a>
                     <a class="navbar-brand" href="index.html">
                         <!-- Logo icon -->
                         <b class="logo-icon p-l-10" style="padding-left: 5px;">
